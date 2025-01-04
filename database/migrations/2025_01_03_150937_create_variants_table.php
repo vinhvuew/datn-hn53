547d96fc4c3ebe1e3be9f36eca1b0d2d6ba037f6
Vinh <vinh@gmail.com>
2025-01-04 20:40:48 +0700

    ok

--- database/migrations/2025_01_03_150937_create_variants_table.php
+++ database/migrations/2025_01_03_150937_create_variants_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
         Schema::create('variants', function (Blueprint $table) {
             $table->id(); // Tạo cột 'id' tự động tăng và là khóa chính
             
-            // $table->foreignIdFor(Products::class)->constrained(); 
-            // $table->foreignIdFor(color::class)->constrained(); 
-            // $table->foreignIdFor(size::class)->constrained(); 
+            $table->foreignIdFor(Products::class)->constrained(); 
+            $table->foreignIdFor(color::class)->constrained(); 
+            $table->foreignIdFor(size::class)->constrained(); 
             $table->decimal('price', 10, 2); // Tạo cột 'price' kiểu số thập phân với 10 chữ số, 2 chữ số thập phân
             $table->integer('quantity'); // Tạo cột 'quantity' kiểu số nguyên
             $table->string('sku')->unique(); // Tạo cột 'sku' kiểu chuỗi, yêu cầu duy nhất
