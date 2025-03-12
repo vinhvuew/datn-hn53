f3e969e69c5a79bb5f453567dfe87a3e93c67eaa
vu vinh <zzvzz9v@gmail.com>
2025-03-12 21:33:38 +0700

    tao bang news

---
+++ app/Models/create_news.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class create_news extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2025_03_12_212259_create_create_news_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title'); // Tiêu đề bài viết
+            $table->text('content'); // Nội dung bài viết
+            $table->string('image')->nullable(); // Đường dẫn ảnh (có thể null)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
