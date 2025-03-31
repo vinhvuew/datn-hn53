b601c97552cbec9fd55ab578869701f8d17bf883
dangtruong163 <trantruong16324@gmail.com>
2025-03-31 22:11:15 +0700

    sua lai logo

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,7 +60,7 @@ class User extends Authenticatable
     }
     public function role()
     {
-        return $this->belongsTo(Role::class,'role_id');
+        return $this->belongsTo(Role::class,);
     }
       // Kiểm tra user có phải admin không
      
