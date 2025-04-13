d2ce163ab0c36f0950877e20da0cfcddde5a7d50
dangtruong163 <trantruong16324@gmail.com>
2025-04-13 21:28:51 +0700

    update fix loi dashboard

--- storage/framework/views/1be681f72f385ed1b7b0644029156ab6.php
+++
@@ -1,7 +0,0 @@
-<script src="<?php echo e(asset('admin')); ?>/assets/js/app-ecommerce-category-list.js"></script>
-<script src="<?php echo e(asset('admin')); ?>/assets/js/app-ecommerce-product-list.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="//cdn.datatables.net/2.2.1/css/dataTables.dataTables.min.css"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="<?php echo e(asset('admin')); ?>/assets/js/app-ecommerce-product-add.js"></script>
-<?php /**PATH D:\laragon\www\datn-hn53\resources\views/admin/layouts/parials/js.blade.php ENDPATH**/ ?>
